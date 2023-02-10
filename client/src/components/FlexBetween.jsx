1e4ff6bde41bd780255510cc0f674fa0aa14b50a
Saurav Raj <saurav.raj5592+git@gmail.com>
2023-02-10 22:10:05 +0530

    Add navbar, login and register page

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,12 @@
+// style component to reuse
+
+import { styled } from "@mui/system";
+import { Box } from "@mui/material";
+
+const FlexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FlexBetween;
